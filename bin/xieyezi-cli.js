4ced629420fd3dd1113e892e3cce7bc556b9ab07
xieyezi <suyechun@iwubida.com>
2019-12-31 12:49:31 +0800

    refactor🛠: 不使用rollup打包

--- bin/xieyezi-cli.js
+++ bin/xieyezi-cli.js
@@ -1,67 +1,90 @@
 #!/usr/bin/env node
-!(function(e) {
-  'function' == typeof define && define.amd ? define(e) : e()
-})(function() {
-  'use strict'
-  var e,
-    o,
-    n = require('../package.json'),
-    r = require('chalk'),
-    i = require('commander'),
-    s = require('semver'),
-    t = n.engines.node,
-    a = require('./lib/create'),
-    c = require('./lib/enhanceErrorMessages'),
-    u = require('didyoumean')
-  ;(u.threshold = 0.6),
-    (e = t),
-    (o = '@xieyezi/cli'),
-    s.satisfies(process.version, e) ||
-      (console.log(
-        r.red(
-          'You are using Node ' +
-            process.version +
-            ', but this version of ' +
-            o +
-            ' requires Node ' +
-            e +
-            '.\nPlease upgrade your Node version.'
-        )
-      ),
-      process.exit(1)),
-    i.version(n.version).usage('<command> [options]'),
-    i
-      .command('create <app-name>')
-      .description('  Create a project with template from xieyezi react template.')
-      .action(function(e, o) {
-        a(e)
-      }),
-    i.arguments('<command>').action(function(e) {
-      i.outputHelp(),
-        console.log('  ' + r.red('Unknown command ' + r.yellow(e))),
-        console.log(),
-        (function(e) {
-          var o = i.commands.map(function(e) {
-              return e._name
-            }),
-            n = u(e, o)
-          n && console.log('  ' + r.red('Did you mean ' + r.yellow(n) + '?'))
-        })(e)
-    }),
-    c('missingArgument', function(e) {
-      return 'Missing required argument ' + r.yellow('<' + e + '>') + '.'
-    }),
-    c('unknownOption', function(e) {
-      return 'Unknown option ' + r.yellow(e) + '.'
-    }),
-    c('optionMissingArgument', function(e, o) {
-      return 'Missing required argument for option ' + r.yellow(e.flags) + (o ? ', got ' + r.yellow(o) : '')
-    }),
-    i.parse(process.argv),
-    i.on('--help', function() {
-      console.log(),
-        console.log('  Run ' + r.cyan('xieyezi-cli <command> --help') + ' for detailed usage of given command.'),
-        console.log()
-    }),
-    process.argv.slice(2).length || i.outputHelp()
+const config = require('../package.json')
+const chalk = require('chalk')
+const program = require('commander')
+const semver = require('semver')
+const requiredVersion = config.engines.node
+const create = require('../lib/create')
+const enhanceErrorMessages = require('../lib/enhanceErrorMessages')
+const didYouMean = require('didyoumean')
+// Setting edit distance to 60% of the input string's length
+didYouMean.threshold = 0.6
+
+function checkNodeVersion(wanted, id) {
+  if (!semver.satisfies(process.version, wanted)) {
+    console.log(
+      chalk.red(
+        'You are using Node ' +
+          process.version +
+          ', but this version of ' +
+          id +
+          ' requires Node ' +
+          wanted +
+          '.\nPlease upgrade your Node version.'
+      )
+    )
+    process.exit(1)
+  }
+}
+/**
+ * Check node version required >=9.0
+ */
+checkNodeVersion(requiredVersion, '@xieyezi/cli')
+/**
+ * start create app
+ */
+program.version(config.version).usage('<command> [options]')
+program
+  .command('create <app-name>')
+  .description('  Create a project with template from xieyezi react template.')
+  .action((name, cmd) => {
+    create(name)
+  })
+/***
+ * output help information on unknown commands
+ */
+program.arguments('<command>').action((cmd) => {
+  program.outputHelp()
+  console.log('  ' + chalk.red(`Unknown command ${chalk.yellow(cmd)}`))
+  console.log()
+  suggestCommands(cmd)
 })
+
+/**
+ * enhance common error messages
+ */
+enhanceErrorMessages('missingArgument', (argName) => {
+  return `Missing required argument ${chalk.yellow(`<${argName}>`)}.`
+})
+
+enhanceErrorMessages('unknownOption', (optionName) => {
+  return `Unknown option ${chalk.yellow(optionName)}.`
+})
+
+enhanceErrorMessages('optionMissingArgument', (option, flag) => {
+  return (
+    `Missing required argument for option ${chalk.yellow(option.flags)}` + (flag ? `, got ${chalk.yellow(flag)}` : '')
+  )
+})
+
+program.parse(process.argv)
+/**
+ * add some useful info on help
+ */
+program.on('--help', () => {
+  console.log()
+  console.log(`  Run ${chalk.cyan('xieyezi-cli <command> --help')} for detailed usage of given command.`)
+  console.log()
+})
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
+function suggestCommands(cmd) {
+  const availableCommands = program.commands.map((cmd) => {
+    return cmd._name
+  })
+  const suggestion = didYouMean(cmd, availableCommands)
+  if (suggestion) {
+    console.log('  ' + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
+  }
+}
